aa2b66322b7e2bebb536347519672349e774ba14
Jianfei Zheng <jiz2@hi.is>
2015-12-22 16:43:58 +0000

    added layouts and templates

--- routes/routes.js
+++ routes/routes.js
@@ -6,14 +6,29 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	/*jshint unused:false*/
-	// res.render('index', { title: 'Express' });
-	res.redirect('/weebly');
+	// res.render('index', {
+	// 	title: 'Atlas Empire Home'
+	// });
+	// res.redirect('/weebly');
+	res.redirect('/battle');
 });
 
+/* GET battlesimulator page. */
+router.get('/battle', function(req, res, next) {
+	/*jshint unused:false*/
+	res.render('battle', {
+		title: 'Atlas Empire Battle Simulator',
+		cssSrc: '/stylesheets/battle.css',
+		jsSrc: '/javascripts/battle.js'
+	});
+});
+
+/* GET weebly page. */
 router.get('/weebly', function(req, res, next) {
 	/*jshint unused:false*/
 	res.render('weeblyscript', {
 		title: 'Atlas Empire Combat Power Calculator',
+		cssSrc: '/stylesheets/weebly.css',
 		cpscript: '/javascripts/calculator.js'
 	});
 });
